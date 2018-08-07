12857bd165bd08ceeb6afdfbb509b4d0350656d8
Sung Bae <sung.bae@canterbury.ac.nz>
2018-08-07 13:34:32 +1200

    runscript added

---
+++ test/runscript.sh
@@ -0,0 +1,13 @@
+echo "im calc tests"
+cd /home/tester/IM_calculation/test
+DIRS='test_*'
+curdir=$PWD
+for DIR in $DIRS
+do
+cd ${curdir}/${DIR}
+pytest --junitxml ${DIR}.xml
+cp -r ./${DIR}.xml /home/tester/.jenkins/workspace/im_calc_tests/${DIR}.xml
+done
+LOG=/home/tester/.jenkins/jobs/im_calc_tests/builds/$BUILD_NUMBER/log
+cp ${LOG} ${LOG}.html
+sed -i 's/$/<br>/' ${LOG}.html
