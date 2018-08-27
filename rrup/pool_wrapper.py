5e67ff60dba3d4658fa7035e955c805d370a35ac
Yen-KO Huang <jonney.huang@canterbury.ac.nz>
2018-08-27 02:16:46 +0000

    pool_wrapper fix: no longer calls pool when np==1

--- rrup/pool_wrapper.py
+++ rrup/pool_wrapper.py
@@ -5,11 +5,14 @@ class PoolWrapper:
     def __init__(self, number_of_process=1, debug_mode=False):
         self.np = number_of_process
         self.debug_mode = debug_mode
-        self.pool = Pool(self.np)
+        if self.debug_mode or self.np == 1:
+            self.pool = None
+        else:
+            self.pool = Pool(self.np)
 
     def map(self, function, iterable_list):
         result = []
-        if self.debug_mode or self.np == 1:
+        if self.pool is None:
             print "Ignoring number of processes here, executing for loop"
             for item in iterable_list:
                 result.append(function(item))
