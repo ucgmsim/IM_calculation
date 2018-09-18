8a8bfac18f3a2cc0818270165ad8bbbf33b52bcd
ttgenius <melodyzyz@gmail.com>
2018-09-18 05:23:31 +0000

    now reads header template and takes header variables like time cpu etc

--- generate_sl.py
+++ generate_sl.py
@@ -11,14 +11,23 @@ import time
 from datetime import datetime
 import checkpoint
 from qcore import utils
-import subprocess
+import re
 
 TEMPLATE_NAME = 'im_calc_sl.template'
+HEADER_NAME = 'slurm_header.cfg'
 TIME = '00:30:00'
 DEFAULT_N_PROCESSES = 40
 DEFAULT_RRUP_OUTDIR = os.path.join('/home', getpass.getuser(), 'imcalc_rrup_out_{}'.format(
     datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H-%M-%S')))
 PARAMS_BASE = 'params_base.py'
+VERSION = 'slurm'
+JOB = 'im_calc'
+ACCOUNT = 'nesi00213'
+CPUS = 1
+EXE_TIME = '%j'
+MAIL = 'test@test.com'
+MEMORY = '90G'
+ADDI = '#SBATCH --hint=nomultithread'
 
 
 # TODO: calculate wall-clock time
@@ -31,21 +40,27 @@ PARAMS_BASE = 'params_base.py'
 # python generate_sl.py -o ~/test_obs/IMCalcExample  -ll /scale_akl_nobackup/filesets/transit/nesi00213/StationInfo/non_uniform_whole_nz_with_real_stations-hh400_v18p6.ll -ml 1000 -simple -e
 
 
-def generate_sl(sim_dirs, obs_dirs, station_file, rrup_files, output_dir, prefix, i, np, extended, simple):
+def generate_sl(sim_dirs, obs_dirs, station_file, rrup_files, output_dir, prefix, i, np, extended, simple, version,
+                job_description, job_name, account, nb_cpus, wallclock_limit, exe_time, mail, memory, additional_lines):
     path = os.path.dirname(os.path.abspath(__file__))
     j2_env = Environment(loader=FileSystemLoader(path), trim_blocks=True)
 
+    header = j2_env.get_template(HEADER_NAME).render(version=version, job_description=job_description,
+                                                     job_name=job_name, account=account, nb_cpus=nb_cpus,
+                                                     wallclock_limit=wallclock_limit, exe_time=exe_time, mail=mail, memory=memory,
+                                                     additional_lines=additional_lines)
     context = j2_env.get_template(TEMPLATE_NAME).render(
-        time=TIME,
+        time=time,
         sim_dirs=sim_dirs, obs_dirs=obs_dirs,
         rrup_files=rrup_files, station_file=station_file,
         output_dir=output_dir, np=np, extended=extended, simple=simple)
     sl_name = '{}_im_calc_{}.sl'.format(prefix, i)
-    return sl_name, context
+    return sl_name, "{}\n{}c".format(header, context)
 
 
 def write_sl(name_context_list):
     for sl_name, context in name_context_list:
+        print("sl_name, ontext", sl_name, context)
         with open(sl_name, 'w') as sl:
             print("writing {}".format(sl_name))
             sl.write(context)
@@ -60,7 +75,9 @@ def get_fault_name(run_name):
 
 
 def split_and_generate_slurms(sim_dirs, obs_dirs, station_file, rrup_files, output_dir, processes, max_lines, prefix,
-                              extended='', simple=''):
+                              extended='', simple='', version=VERSION, job_description='', job_name=JOB,
+                              account=ACCOUNT, nb_cpus=CPUS, wallclock_limit=TIME, exe_time=EXE_TIME, mail=MAIL,
+                              memory=MEMORY, additional_lines=ADDI):
     total_dir_lines = 0
     if sim_dirs != []:
         total_dir_lines = len(sim_dirs)
@@ -76,7 +93,8 @@ def split_and_generate_slurms(sim_dirs, obs_dirs, station_file, rrup_files, outp
         if 0 <= last_line_index - total_dir_lines <= max_lines:
             last_line_index = total_dir_lines
         name, context = generate_sl(sim_dirs[i: last_line_index], obs_dirs[i: last_line_index], station_file,
-                                    rrup_files[i: last_line_index], output_dir, prefix, i, processes, extended, simple)
+                                    rrup_files[i: last_line_index], output_dir, prefix, i, processes, extended, simple, version,
+                job_description, job_name, account, nb_cpus, wallclock_limit, exe_time, mail, memory, additional_lines)
         name_context_list.append((name, context))
         i += max_lines
 
@@ -105,7 +123,6 @@ def get_dirs(run_folder, arg_identifiers, com_pattern):
         dir_path = glob.glob(os.path.join(run_folder, com_pattern.format(fault_name, identifier)))
         print("id dir_path", dir_path)
         dirs += dir_path
-    print(dirs)
     return dirs
 
 
@@ -127,8 +144,21 @@ def main():
     parser.add_argument('-s', '--simple_output', action='store_const', const='-s', default='',
                         help="Please add '-s' to indicate if you want to output the big summary csv only(no single station csvs). Default outputting both single station and the big summary csvs")
     parser.add_argument('-o', '--rrup_out_dir', default=DEFAULT_RRUP_OUTDIR,
-                        help="output directory to store rupture distances output.Default is {}".format(DEFAULT_RRUP_OUTDIR))
-    parser.add_argument('-i', '--identifiers', default='*', nargs='+', help="a list of space-seperated unique runnames of the simulations.eg.'Albury_HYP01-01_S1244 OpouaweUruti_HYP44-47_S1674'")
+                        help="output directory to store rupture distances output.Default is {}".format(
+                            DEFAULT_RRUP_OUTDIR))
+    parser.add_argument('-i', '--identifiers', default='*', nargs='+',
+                        help="a list of space-seperated unique runnames of the simulations.eg.'Albury_HYP01-01_S1244 OpouaweUruti_HYP44-47_S1674'")
+    parser.add_argument('-t', '--time', default=TIME,
+                        help="estimated running time for each slurm sciprt. must be in 'hh:mm:ss' format. Default is '00:30:00'")
+    parser.add_argument('--version', default=VERSION, help="default version is 'slurm'")
+    parser.add_argument('--job_description', default=JOB)
+    parser.add_argument('--job_name', default=JOB)
+    parser.add_argument('--account', default=ACCOUNT)
+    parser.add_argument('--ntasks', default=CPUS)
+    parser.add_argument('--exe_time', default=EXE_TIME)
+    parser.add_argument('--mail', default=MAIL)
+    parser.add_argument('--memory', default=MEMORY)
+    parser.add_argument('--additional_lines', default=ADDI)
 
     args = parser.parse_args()
 
@@ -139,19 +169,26 @@ def main():
         parser.error(
             "-ml argument should come with a number that is 0 < -ml <= (max_lines-header_and_other_prints) allowed by slurm")
 
+    if not re.match('(24:00:00)|(^(2[0-3]|[01]?[0-9]):([0-5]?[0-9]):([0-5]?[0-9])$)', args.time):
+        parser.error("time must be in 'hh:mm:ss' format and not exceeding 24:00:00")
+
     # sim_dir = /nesi/nobackup/nesi00213/RunFolder/Cybershake/v18p5/Runs
     if args.sim_dir is not None:
         print("Args.sim_dir", args.sim_dir)
         sim_waveform_dirs = get_dirs(args.sim_dir, args.identifiers, '{}/BB/*/{}')
-        print("sim_waveform_dirs",sim_waveform_dirs)
+        print("sim_waveform_dirs", sim_waveform_dirs)
         sim_waveform_dirs = checkpoint.checkpoint_sim_obs(sim_waveform_dirs,
                                                           '../../../IM_calc/')  # return dirs that are not calculated yet
         sim_run_names = map(os.path.basename, sim_waveform_dirs)
         sim_faults = map(get_fault_name, sim_run_names)
         sim_dirs = zip(sim_waveform_dirs, sim_run_names, sim_faults)
         # sim
-        name_context_list = split_and_generate_slurms(sim_dirs, [], args.station_file, [], args.rrup_out_dir, args.processes,
-                                  args.max_lines, 'sim', extended=args.extended_period, simple=args.simple_output)
+        name_context_list = split_and_generate_slurms(sim_dirs, [], args.station_file, [], args.rrup_out_dir,
+                                                      args.processes,
+                                                      args.max_lines, 'sim', extended=args.extended_period,
+                                                      simple=args.simple_output, version=args.version, job_description=args.job_description, job_name=args.job_name,
+                              account=args.account, nb_cpus=args.ntasks, wallclock_limit=args.time, exe_time=args.exe_time, mail=args.mail,
+                              memory=args.memory, additional_lines=args.additional_lines)
         write_sl(name_context_list)
 
     if args.srf_dir is not None:
@@ -165,18 +202,26 @@ def main():
                 run_name = get_basename_without_ext(srf_file)
                 rrup_files.append((srf_file, run_name, fd))
         # rrup
-        name_context_list = split_and_generate_slurms([], [], args.station_file, rrup_files, args.rrup_out_dir, args.processes,args.max_lines, 'rrup')
+        name_context_list = split_and_generate_slurms([], [], args.station_file, rrup_files,
+                                                      args.rrup_out_dir, args.processes, args.max_lines, 'rrup', version=args.version, job_description=args.job_description, job_name=args.job_name,
+                              account=args.account, nb_cpus=args.ntasks, wallclock_limit=args.time, exe_time=args.exe_time, mail=args.mail,
+                              memory=args.memory, additional_lines=args.additional_lines)
         write_sl(name_context_list)
 
     if args.obs_dir is not None:
         obs_waveform_dirs = glob.glob(os.path.join(args.obs_dir, '*'))
         obs_waveform_dirs = checkpoint.checkpoint_sim_obs(obs_waveform_dirs, '../IM_calc/')
+        print("obs_waveform_dirs", obs_waveform_dirs)
         obs_run_names = map(os.path.basename, obs_waveform_dirs)
         obs_faults = map(get_fault_name, obs_run_names)
         obs_dirs = zip(obs_waveform_dirs, obs_run_names, obs_faults)
         # obs
-        name_context_list = split_and_generate_slurms([], obs_dirs, args.station_file, [], args.rrup_out_dir, args.processes,
-                                  args.max_lines, 'obs', extended=args.extended_period, simple=args.simple_output)
+        name_context_list = split_and_generate_slurms([], obs_dirs, args.station_file, [], args.rrup_out_dir,
+                                                      args.processes,
+                                                      args.max_lines, 'obs', extended=args.extended_period,
+                                                      simple=args.simple_output, version=args.version, job_description=args.job_description, job_name=args.job_name,
+                              account=args.account, nb_cpus=args.ntasks, wallclock_limit=args.time, exe_time=args.exe_time, mail=args.mail,
+                              memory=args.memory, additional_lines=args.additional_lines)
         write_sl(name_context_list)
 
 
