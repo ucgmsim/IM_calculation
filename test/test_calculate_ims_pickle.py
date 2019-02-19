50590c44dc395b74b446796c96f7623546607b47
jpa198 <james.paterson@canterbury.ac.nz>
2019-02-20 10:58:34 +1300

    Added sample test

---
+++ test/test_calculate_ims_pickle.py
@@ -0,0 +1,17 @@
+import pickle
+import calculate_ims
+import os.path
+
+
+test_data_save_dir = '/home/jpa198/test_space/im_calc_test/pickled/Hossack_HYP01-10_S1244'
+REALISATION = 'Hossack_HYP01-10_S1244'
+
+def test_convert_str_comp():
+    function = 'convert_str_comp'
+    with open(os.path.join(test_data_save_dir, function + '_comp.P'), 'rb') as load_file:
+        comp = pickle.load(load_file)
+    value_to_test = calculate_ims.convert_str_comp(comp)
+    with open(os.path.join(test_data_save_dir, function + '_converted_comp.P'), 'rb') as load_file:
+        converted_comp = pickle.load(load_file)
+    assert value_to_test == converted_comp
+
