253087e5f4782460f4c41eed8643b9f941422b58
jpa198 <james.paterson@canterbury.ac.nz>
2019-02-20 16:12:15 +1300

    Removed unused data files Moved set_up to be a session global function Fixed test_get_comp_name_and_list

--- test/test_calculate_ims/test_calculate_ims.py
+++ test/test_calculate_ims/test_calculate_ims.py
@@ -44,53 +44,53 @@ def test_validate_period_fail(test_period, test_extended, test_im):
     with pytest.raises(SystemExit):
         calculate_ims.validate_period(PARSER, test_period, test_extended, test_im)
 
-class TestPickleTesting():
+REALISATIONS = [('PangopangoF29_HYP01-10_S1244',"https://www.dropbox.com/sh/dgpfukqd01zucjv/AAA8iMASZWn5vbr0PdDCgTG3a?dl=0")]
+test_data_save_dirs = []
 
-    REALISATIONS = [('PangopangoF29_HYP01-10_S1244',"https://www.dropbox.com/sh/dgpfukqd01zucjv/AAA8iMASZWn5vbr0PdDCgTG3a?dl=0")]
-    test_data_save_dirs = []
+# Run this once, but run it for any test/collection of tests that is run in this class
+@pytest.fixture(scope='session', autouse=True)
+def set_up(self):
+    for i, (REALISATION, DATA_DOWNLOAD_PATH) in enumerate(self.REALISATIONS):
+        DATA_STORE_PATH = os.path.join(".", "sample"+str(i))
 
-    # Run this once, but run it for any test/collection of tests that is run in this class
-    @pytest.fixture(scope='class', autouse=True)
-    def set_up(self):
-        for i, (REALISATION, DATA_DOWNLOAD_PATH) in enumerate(self.REALISATIONS):
-            DATA_STORE_PATH = os.path.join(".", "sample"+str(i))
-            os.mkdir(DATA_STORE_PATH)
+        ZIP_DOWNLOAD_PATH = os.path.join(DATA_STORE_PATH, REALISATION+".zip")
+        OUTPUT_DIR_PATH = os.path.join(DATA_STORE_PATH, "input")
+        os.makedirs(OUTPUT_DIR_PATH, exist_ok=True)
 
-            ZIP_DOWNLOAD_PATH = os.path.join(DATA_STORE_PATH, REALISATION+".zip")
-            OUTPUT_DIR_PATH = os.path.join(DATA_STORE_PATH, "input")
-            os.mkdir(OUTPUT_DIR_PATH)
+        DOWNLOAD_CMD = "wget -O {} {}".format(ZIP_DOWNLOAD_PATH, DATA_DOWNLOAD_PATH)
+        UNZIP_CMD = "unzip {} -d {}".format(ZIP_DOWNLOAD_PATH, OUTPUT_DIR_PATH)
 
-            DOWNLOAD_CMD = "wget -O {} {}".format(ZIP_DOWNLOAD_PATH, DATA_DOWNLOAD_PATH)
-            UNZIP_CMD = "unzip {} -d {}".format(ZIP_DOWNLOAD_PATH, OUTPUT_DIR_PATH)
-
-            self.test_data_save_dirs.append(OUTPUT_DIR_PATH)
-            if not os.path.isfile(OUTPUT_DIR_PATH):
-                out, err = shared.exe(DOWNLOAD_CMD, debug=False)
-                if b"failed" in err:
-                    os.remove(ZIP_DOWNLOAD_PATH)
-                    sys.exit("{} failed to download data folder".format(err))
-                else:
-                    print("Successfully downloaded benchmark data folder")
-
-                out, err = shared.exe(UNZIP_CMD, debug=False)
+        self.test_data_save_dirs.append(OUTPUT_DIR_PATH)
+        if not os.path.isfile(OUTPUT_DIR_PATH):
+            out, err = shared.exe(DOWNLOAD_CMD, debug=False)
+            if b"failed" in err:
                 os.remove(ZIP_DOWNLOAD_PATH)
-                if b"error" in err:
-                    shutil.rmtree(OUTPUT_DIR_PATH)
-                    sys.exit("{} failed to extract data folder".format(err))
+                sys.exit("{} failed to download data folder".format(err))
             else:
-                print("Benchmark data folder already exits")
+                print("Successfully downloaded benchmark data folder")
 
-        # Run all tests
-        yield
+            out, err = shared.exe(UNZIP_CMD, debug=False)
+            os.remove(ZIP_DOWNLOAD_PATH)
+            if b"error" in err:
+                shutil.rmtree(OUTPUT_DIR_PATH)
+                sys.exit("{} failed to extract data folder".format(err))
+        else:
+            print("Benchmark data folder already exits")
 
-        # Remove the test data directory
-        for _, PATH in self.REALISATIONS:
-            shutil.rmtree(PATH)
+    # Run all tests
+    yield
+
+    # Remove the test data directory
+    for PATH in self.test_data_save_dirs:
+        shutil.rmtree(PATH)
+
+
+class TestPickleTesting():
 
     def test_convert_str_comp(self):
 
         function = 'convert_str_comp'
-        for root_path in self.test_data_save_dirs:
+        for root_path in test_data_save_dirs:
 
             with open(os.path.join(root_path, function + '_comp.P'), 'rb') as load_file:
                 comp = pickle.load(load_file)
@@ -107,7 +107,7 @@ class TestPickleTesting():
     def test_get_comp_name_and_list(self):
 
         function = 'get_comp_name_and_list'
-        for root_path in self.test_data_save_dirs:
+        for root_path in test_data_save_dirs:
             with open(os.path.join(root_path, function + '_comp.P'), 'rb') as load_file:
                 comp = pickle.load(load_file)
             with open(os.path.join(root_path, function + '_geom_only.P'), 'rb') as load_file:
@@ -115,11 +115,11 @@ class TestPickleTesting():
 
             value1_to_test, value2_to_test = calculate_ims.get_comp_name_and_list(comp, geom_only)
 
-            with open(os.path.join(root_path, function + '_converted_comp.P'), 'rb') as load_file:
-                converted_comp = pickle.load(load_file)
-            with open(os.path.join(root_path, function + '_converted_comp.P'), 'rb') as load_file:
-                converted_comp2 = pickle.load(load_file)
+            with open(os.path.join(root_path, function + '_comp_name.P'), 'rb') as load_file:
+                comp_name = pickle.load(load_file)
+            with open(os.path.join(root_path, function + '_comps.P'), 'rb') as load_file:
+                comps = pickle.load(load_file)
 
-            assert value1_to_test == converted_comp
-            assert value2_to_test == converted_comp2
+            assert value1_to_test == comp_name
+            assert value2_to_test == comps
 
