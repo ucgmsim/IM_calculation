4e30221d7e25dd2d2fd1e6b4270900b3015a52ed
jpa198 <james.paterson@canterbury.ac.nz>
2019-02-20 13:23:23 +1300

    Added set up and tear down functions. Each runs once per test session.

--- test/test_calculate_ims_pickle.py
+++ test/test_calculate_ims_pickle.py
@@ -1,35 +1,49 @@
 import pickle
+
+import pytest
+import shutil
+
 import calculate_ims
 import os
 import getpass
 import sys
 from qcore import shared
 
-test_data_save_dir = 'pickled_test_data'
+
 REALISATION = 'PangopangoF29_HYP01-10_S1244'
 
 DATA_DOWNLOAD_PATH = "https://www.dropbox.com/sh/dgpfukqd01zucjv/AAA8iMASZWn5vbr0PdDCgTG3a?dl=0"
-DATA_STORE_PATH = os.path.join("/home", getpass.getuser(), REALISATION+'.zip')
-DOWNLOAD_CMD = "wget -O {} {}".format(DATA_STORE_PATH, DATA_DOWNLOAD_PATH)
-UNZIP_CMD = "unzip {} -d {}".format(REALISATION+'.zip', REALISATION)
+DATA_STORE_PATH = os.path.join("/home", getpass.getuser(), REALISATION)
+DOWNLOAD_CMD = "wget -O {} {}".format(DATA_STORE_PATH+'.zip', DATA_DOWNLOAD_PATH)
+UNZIP_CMD = "unzip {} -d {}".format(os.path.join("/home", getpass.getuser(), REALISATION+'.zip'), os.path.join("/home", getpass.getuser(), REALISATION))
+
+test_data_save_dir = os.path.join("/home", getpass.getuser(), REALISATION)
 
 
-def test_set_up():
+@pytest.fixture(scope='session', autouse=True)
+def set_up():
     if not os.path.isfile(DATA_STORE_PATH):
         out, err = shared.exe(DOWNLOAD_CMD, debug=False)
         if b"failed" in err:
-            os.remove(DATA_STORE_PATH)
+            os.remove(DATA_STORE_PATH+'.zip')
             sys.exit("{} failed to download data folder".format(err))
         else:
             print("Successfully downloaded benchmark data folder")
+
         out, err = shared.exe(UNZIP_CMD, debug=False)
-        if b"failed" in err:
-            os.remove(DATA_STORE_PATH)
+        os.remove(DATA_STORE_PATH+'.zip')
+        if b"error" in err:
+            os.remove(DATA_STORE_PATH + '.zip')
+            shutil.rmtree(DATA_STORE_PATH)
             sys.exit("{} failed to extract data folder".format(err))
-        else:
-            print("Successfully extracted data folder")
     else:
         print("Benchmark data folder already exits")
+
+
+@pytest.fixture(scope='session', autouse=True)
+def tear_down():
+    yield
+    shutil.rmtree(DATA_STORE_PATH)
 
 
 def test_convert_str_comp():
