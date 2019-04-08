318f415f6e19b8dd7592b5f475f3a53911341257
jpa198 <james.paterson@canterbury.ac.nz>
2019-04-08 16:33:11 +1200

    Update location of remote data

--- test/test_common_set_up.py
+++ test/test_common_set_up.py
@@ -16,7 +16,7 @@ OUTPUT = "output"
 REALISATIONS = [
     (
         "PangopangoF29_HYP01-10_S1244",
-        "http://ec2-54-206-55-199.ap-southeast-2.compute.amazonaws.com/static/public/testing/PangopangoF29_HYP01-10_S1244.zip",
+        "http://ec2-13-211-174-16.ap-southeast-2.compute.amazonaws.com:5000//static/public/testing/PangopangoF29_HYP01-10_S1244.zip",
     )
 ]
 
