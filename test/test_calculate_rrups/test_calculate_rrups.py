7c0afc13e95c13f9a7172b808b27ef4aa58d887a
ttgenius <melodyzyz@gmail.com>
2019-04-10 13:54:31 +1200

    added test

--- test/test_calculate_rrups/test_calculate_rrups.py
+++ test/test_calculate_rrups/test_calculate_rrups.py
@@ -9,26 +9,10 @@ import calculate_rrups
 from test.test_common_set_up import INPUT, OUTPUT, set_up, get_bench_output, get_input_params
 
 
-def test_write_and_calculate_rrups(set_up):
-    func_name = 'write_and_calculate_rrups'
-    params = inspect.getfullargspec(calculate_rrups.write_and_calculate_rrups).args
-    for root_path in set_up:
-        input_params = get_input_params(root_path, func_name, params)
-        test_output = calculate_rrups.write_and_calculate_rrups(*input_params)
-        pass
-        # missing pickled benchmark output data
-
-
 def test_get_fd_stations(set_up):
-    func_name = 'get_fd_station'
+    func_name = 'get_fd_stations'
     for root_path in set_up:
-        with open(
-                os.path.join(root_path, INPUT, func_name + "_fd.ll.P"), "rb"
-        ) as load_file:
-            fd_ll = pickle.load(load_file)
-
-        test_output = calculate_rrups.get_fd_stations(fd_ll)
-
+        test_output = calculate_rrups.get_fd_stations(os.path.join(root_path, INPUT, "sample_station.ll"))
         with open(
                 os.path.join(root_path, OUTPUT, func_name + "_stations.P"), "rb"
         ) as load_file:
@@ -56,5 +40,5 @@ def test_get_match_stations(set_up):
         ) as load_file:
             bench_output = pickle.load(load_file)
 
-        assert test_output ==  bench_output
+        assert test_output == bench_output
 
