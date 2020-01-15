a933b5a8f346ba359991f1a4be1ee23041af33f5
jpa198 <james.paterson@canterbury.ac.nz>
2020-01-15 14:51:07 +1300

    Change konno matrix creation to go in the IM directory

--- travis_setup.py
+++ travis_setup.py
@@ -1,45 +1,9 @@
 """
-Install using pip, e.g. pip install ./IM_Calculation
-use --no-deps to prevent re-installation of dependencies
-use -I to force re-install
+Generates the konno matrices in the IM directory
 """
-from setuptools import find_packages
-from distutils.core import setup
-from distutils.command.build_py import build_py
-from distutils.extension import Extension
 
-import numpy
+import os
+from IM_calculation.scripts.A_KonnoMatricesComputation import createKonnoMatrices
 
+createKonnoMatrices(os.path.join(os.getcwd(), "IM_calculation", "IM", "KO_matrices"), True, 5)
 
-class build_konno_matricies(build_py):
-    """Post-installation for development mode."""
-
-    def run(self):
-        from IM_calculation.scripts.A_KonnoMatricesComputation import (
-            createKonnoMatrices,
-        )
-
-        createKonnoMatrices(self.build_lib, True, 5)
-        build_py.run(self)
-
-
-setup(
-    name="IM-calc",
-    version="19.5.1",
-    packages=find_packages(),
-    url="https://github.com/ucgmsim/IM_calculation",
-    description="IM calculation code",
-    install_requires=["numpy>=1.14.3", "numba>=0.43.1", "Cython", "pandas"],
-    cmdclass={"build_py": build_konno_matricies},
-    ext_modules=[
-        Extension(
-            "IM_calculation.IM.rspectra_calculations.rspectra",
-            ["IM_calculation/IM/rspectra_calculations/rspectra.pyx"],
-        )
-    ],
-    scripts=[
-        "IM_calculation/scripts/calculate_ims.py",
-        "IM_calculation/scripts/calculate_rrups_single.py",
-    ],
-    include_dirs=[numpy.get_include()],
-)
