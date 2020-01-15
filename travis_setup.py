05eb435437f6f1de3c394e7c913cae46387401d2
jpa198 <james.paterson@canterbury.ac.nz>
2020-01-15 14:59:40 +1300

    Tidy up Travis set up

--- travis_setup.py
+++ travis_setup.py
@@ -5,7 +5,4 @@ Generates the konno matrices in the IM directory
 import os
 from IM_calculation.scripts.A_KonnoMatricesComputation import createKonnoMatrices
 
-path = os.getcwd()
-print(path)
-createKonnoMatrices(path, True, 5)
-
+createKonnoMatrices(os.getcwd(), True, 5)
