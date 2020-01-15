7804b46a723f34a7a37ec4b340f2d41acbbb3f7f
jpa198 <james.paterson@canterbury.ac.nz>
2020-01-15 14:55:24 +1300

    Show where matrices are being saved to

--- travis_setup.py
+++ travis_setup.py
@@ -5,5 +5,7 @@ Generates the konno matrices in the IM directory
 import os
 from IM_calculation.scripts.A_KonnoMatricesComputation import createKonnoMatrices
 
-createKonnoMatrices(os.path.join(os.getcwd(), "IM_calculation", "IM", "KO_matrices"), True, 5)
+path = os.path.join(os.getcwd(), "IM_calculation", "IM", "KO_matrices")
+print(path)
+createKonnoMatrices(path, True, 5)
 
