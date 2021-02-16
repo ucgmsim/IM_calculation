5a03a826e366969207463a28ce7e5b69b8b83301
Yen-KO Huang <jonney.huang@canterbury.ac.nz>
2021-02-16 03:46:41 +0000

    black

--- IM_calculation/IM/im_calculation.py
+++ IM_calculation/IM/im_calculation.py
@@ -67,6 +67,7 @@ def convert_str_comp(arg_comps):
     else:
         return arg_comps, arg_comps
 
+
 def array_to_dict(value, comps_to_calc, im, comps_to_store):
     """
     convert a numpy arrary that contains calculated im values to a dict {comp: value}
@@ -507,8 +508,8 @@ def compute_measures_multiprocess(
         if not advanced_im_config.IM_list:
             all_results.extend(p.starmap(compute_measure_single, array_params))
             # adv_im changes for ref, TODO:del once test pass
-            #result_list = p.map(compute_measure_single, array_params)
-            #for result in result_list:
+            # result_list = p.map(compute_measure_single, array_params)
+            # for result in result_list:
             #    all_result_dict.update(result)
         if advanced_im_config.IM_list:
             # calculate IM for stations in this iteration
@@ -524,23 +525,22 @@ def compute_measures_multiprocess(
     # write the ouput after all cals are done
     if not advanced_im_config.IM_list:
         # TODO: del after test pass
-        #write_result(
+        # write_result(
         #    all_result_dict, output_dir, identifier, comp, ims, period, simple_output
-        #)
-        
+        # )
+
         all_result_dict = ChainMap(*all_results)
         output_path = get_result_filepath(output, identifier, ".csv")
 
         results_dataframe = pd.DataFrame.from_dict(all_result_dict, orient="index")
         results_dataframe.index = pd.MultiIndex.from_tuples(
             results_dataframe.index, names=["station", "component"]
-        )   
+        )
         results_dataframe.sort_values(["station", "component"], inplace=True)
         results_dataframe = order_im_cols_df(results_dataframe)
 
         # Save the transposed dataframe
         results_dataframe.to_csv(output_path)
-
 
     # write for advanced IM (pandas array)
     if advanced_im_config.IM_list:
