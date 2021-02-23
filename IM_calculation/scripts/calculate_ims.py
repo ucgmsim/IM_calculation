ea24c2ab574a1c5ece80f02d9453320a6f22cfe3
Sung Eun Bae <sung.bae@canterbury.ac.nz>
2021-02-23 14:26:11 +1300

    Added IESDR into the workflow

--- IM_calculation/scripts/calculate_ims.py
+++ IM_calculation/scripts/calculate_ims.py
@@ -73,7 +73,7 @@ def load_args():
         nargs="+",
         default=constants.DEFAULT_PSA_PERIODS,
         type=float,
-        help="Please provide pSA period(s) separated by a space. eg: "
+        help="Please provide pSA or IESDR period(s) separated by a space. eg: "
         "0.02 0.05 0.1. Default periods are: {}".format(
             ",".join(str(v) for v in constants.DEFAULT_PSA_PERIODS)
         ),
@@ -156,6 +156,9 @@ def main():
 
     if "pSA" in im:
         im_options["pSA"] = calc.validate_period(args.period, args.extended_period)
+
+    if "IESDR" in im:
+        im_options["IESDR"] = calc.validate_period(args.period, args.extended_period)
 
     if "FAS" in im:
         im_options["FAS"] = calc.validate_fas_frequency(args.fas_frequency)
