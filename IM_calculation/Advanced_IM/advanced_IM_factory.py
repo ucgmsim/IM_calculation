622dcfe745669bf4e20e4bc3e69ac292cd9730c7
Yen-KO Huang <jonney.huang@canterbury.ac.nz>
2021-03-03 20:16:12 +0000

    black format

--- IM_calculation/Advanced_IM/advanced_IM_factory.py
+++ IM_calculation/Advanced_IM/advanced_IM_factory.py
@@ -90,7 +90,9 @@ def save_waveform_to_tmp_files(tmp_folder, accelerations, station_name):
 def agg_csv(stations, im_calc_dir, im_type):
     # get csv base on station name
     # quick check of args format
-    assert type(im_type) == str, "im_type should be a string, but get {} instead".format(type(im_type))
+    assert (
+        type(im_type) == str
+    ), "im_type should be a string, but get {} instead".format(type(im_type))
     # initial a blank dataframe
     df = pd.DataFrame()
 
