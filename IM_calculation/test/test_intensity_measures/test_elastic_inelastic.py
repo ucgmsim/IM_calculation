b0299ea94c7ec08ee47c737e67721b9389a46cad
Sung Eun Bae <sung.bae@canterbury.ac.nz>
2021-03-05 15:32:10 +1300

    Changed to lowcase variables

--- IM_calculation/test/test_intensity_measures/test_elastic_inelastic.py
+++ IM_calculation/test/test_intensity_measures/test_elastic_inelastic.py
@@ -65,8 +65,8 @@ def test_elastic_inelastic():
             -1.4989352e-07,
         ]
     )
-    Dtg = 0.005
-    Dt = 0.005
+    dtg = 0.005
+    dt = 0.005
     bench = np.asarray(
         [
             1.51521e-12,
@@ -87,5 +87,5 @@ def test_elastic_inelastic():
         ]
     )
 
-    res = Bilinear_Newmark_withTH(periods, z, dy, alpha, acc, Dtg, Dt)
+    res = Bilinear_Newmark_withTH(periods, z, dy, alpha, acc, dtg, dt)
     assert np.allclose(res, bench)
