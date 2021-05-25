a19098a7cc8a2ace9cbe63d28b1e34eba8a774ce
James Paterson <james.paterson@canterbury.ac.nz>
2021-05-25 15:46:57 +1200

    More renaming

--- IM_calculation/IM/intensity_measures.py
+++ IM_calculation/IM/intensity_measures.py
@@ -49,21 +49,21 @@ def calculate_Nstep(DT, NT):
 
 
 def get_rotations(
-    spectral_accelerations,
+    accelerations,
     func=lambda x: np.max(np.abs(x), axis=1),
     delta_theta: int = 1,
     min_angle: int = 0,
     max_angle: int = 180,
 ):
     """
-    Calculates the rotd values for the given spectral accelerations
+    Calculates the rotd values for the given accelerations
     Works across multiple periods at once
     For each angle in the range [min_angle, max_angle) with step size delta theta,
     the acceleration at each timestep is rotated to get the component in the direction of the given angle
     The absolute value is taken as it does not matter if the acceleration is in the direction of the angle,
     or 180 degrees out of phase of it
     For each angle the maximum acceleration is taken.
-    :param spectral_accelerations: An array with shape [periods.size, nt, 2] where nt is the number of timesteps in the original waveform
+    :param accelerations: An array with shape [periods.size, nt, 2] where nt is the number of timesteps in the original waveform
     :param func: The function to be applied to each waveform after rotations have been applied. Default takes the maximum of each angle for each period
     :param delta_theta: The difference between each angle to take. Defaults to 2 degrees
     :param min_angle: The minimum angle in degrees to calculate from, 0 is due East
@@ -73,7 +73,7 @@ def get_rotations(
 
     thetas = np.deg2rad(np.arange(min_angle, max_angle, delta_theta))
     rotation_matrices = np.asarray([np.cos(thetas), np.sin(thetas)])
-    periods, nt, xy = spectral_accelerations.shape
+    periods, nt, xy = accelerations.shape
     rotds = np.zeros((periods, thetas.size))
 
     # Magic number empirically determined from runs on Maui
@@ -82,7 +82,7 @@ def get_rotations(
 
     for period in range(0, periods, step):
         rotds[period : period + step] = func(
-            np.dot(spectral_accelerations[period : period + step], rotation_matrices)
+            np.dot(accelerations[period: period + step], rotation_matrices)
         )
 
     return rotds
