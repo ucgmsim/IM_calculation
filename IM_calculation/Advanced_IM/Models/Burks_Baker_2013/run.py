09c8ab62d5999dbf4c0bd73c07e3875e2b0eb6df
Sung Eun Bae <sung.bae@canterbury.ac.nz>
2021-07-02 13:25:46 +1200

    Support multiple dy values

--- IM_calculation/Advanced_IM/Models/Burks_Baker_2013/run.py
+++ IM_calculation/Advanced_IM/Models/Burks_Baker_2013/run.py
@@ -25,7 +25,7 @@ STORIES = 10
 
 z_list = [0.05]  # damping ratio
 alpha_list = [0.05]  # strain hardening ratio
-dy_list = [0.1765]  # strain hardening ratio
+dy_list = [0.1765, 0.4, 0.6]  # strain hardening ratio
 dt_list = [0.005]
 period = np.array(constants.DEFAULT_PSA_PERIODS)
 
@@ -48,29 +48,45 @@ def main(comp_000, comp_090, output_dir):
     results = {}
 
     accelerations = np.array((waveforms["090"], waveforms["000"])).T
-    all_im_names = []
+    ordered_columns_dict = {}
+
     for z in z_list:
-        for alpha in alpha_list:
-            for dy in dy_list:
-                for dt in dt_list:
+        for dt in dt_list:
+            for alpha in alpha_list:
+                for dy in dy_list:
                     displacements = intensity_measures.get_SDI_nd(
                         accelerations, period, NT, DT, z, alpha, dy, dt
                     )
                     sdi_values = np.max(np.abs(displacements), axis=1)
-                    im_names = ["SDI_{}".format(t) for t in period]
-                    all_im_names.extend(im_names)
+                    im_names = []
+                    for t in period:
+                        im_name = f"SDI_{t}_dy{dy}"  # we only vary dy at the moment
+                        im_names.append(im_name)
+                        # for each period, we will have a list of im_names varying dy. to help ordering columns later
+                        if t not in ordered_columns_dict:
+                            ordered_columns_dict[t] = []
+                        ordered_columns_dict[t].append(im_name)
+
                     for i, component in enumerate(COMPONENT):
-                        results[component] = {}
+                        if component not in results:
+                            results[component] = {}
                         for j, im_name in enumerate(im_names):
-                            results[component][im_name] = sdi_values[j, i] * 100
+                            results[component][im_name] = (
+                                sdi_values[j, i] * 100
+                            )  # Burks & Baker returns m, but output is stored in cm
+
+    ordered_columns = []
+    for t in period:
+        ordered_columns.extend(
+            ordered_columns_dict[t]
+        )  # order by period first, then by dy
 
     im_csv_fname = os.path.join(output_dir, "Burks_Baker_2013.csv")
     df = pd.DataFrame.from_dict(results, orient="index")
     df.index.name = "component"
     geom = pd.Series(get_geom(df.loc["000"], df.loc["090"]), name="geom")
     df = df.append(geom)
-
-    df[all_im_names].to_csv(im_csv_fname)
+    df[ordered_columns].to_csv(im_csv_fname)
 
 
 if __name__ == "__main__":
