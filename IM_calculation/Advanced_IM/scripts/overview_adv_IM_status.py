c2251dfca31bd033514cdf8fe502d93f549f4cf3
Sung Bae <sung.bae@canterbury.ac.nz>
2021-08-13 13:59:54 +1200

    Updated as per reviewers' comments

---
+++ IM_calculation/Advanced_IM/scripts/overview_adv_IM_status.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+import glob
+import os.path
+import pandas as pd
+from pathlib import Path
+import sys
+
+from qcore import simulation_structure
+
+ALL_STATUS = [
+    "finished",
+    "not_started",
+    "not_converged",
+    "not_finished",
+    "timed_out",
+    "crashed",
+    "unknown",
+]
+TYPES = ["sim", "obs", "custom"]
+
+PATTERN_TEMPLATE = {
+    "sim": simulation_structure.get_im_calc_dir("", "*"),
+    "obs": "ObservedGroundMotions/IM_calc/*",
+}
+
+
+def process(type, model, path, outdir, pattern=None, verbose=False):
+    search_pattern = PATTERN_TEMPLATE.get(type, pattern) + f"/{model}_status.csv"
+    print(f"Searching for {path}/{search_pattern}")
+
+    event_status_files = list(path.glob(search_pattern))
+
+    if len(event_status_files) == 0:
+        print(f"Error:No status files found at {path}")
+        print(f"Check the root_path, model and search pattern")
+        sys.exit(0)
+
+    event_status_dict = {}
+    for event_status_file in event_status_files:
+        if type == "sim":
+            event_name = Path(event_status_file).parents[2].name
+        else:
+            event_name = Path(event_status_file).parent.name
+        event_status_df = pd.read_csv(event_status_file)
+        event_status_dict[event_name] = {"total": 0}
+        for status in ALL_STATUS:
+
+            count = event_status_df["status"].value_counts().get(status, 0)
+            event_status_dict[event_name][status] = count
+            event_status_dict[event_name]["total"] += count
+
+    overall_status_df = pd.DataFrame.from_dict(
+        event_status_dict, orient="index", columns=["total"] + ALL_STATUS
+    )
+
+    overall_status_df.to_csv(outdir / f"overall_status_{model}_{type}.csv")
+
+    not_finished_df = overall_status_df.loc[
+        overall_status_df["total"] != overall_status_df["finished"]
+    ]
+    print("------------")
+    print(
+        overall_status_df.sum(axis=0).to_string(header=None)
+    )  # .to_string(header=None) removes dtype: int64
+    print("------------")
+    if len(not_finished_df) > 0:
+        print(
+            f"{len(not_finished_df)} incomplete/unsuccessful event(s) found (Total: {len(overall_status_df)})"
+        )
+        if verbose:
+            print(not_finished_df)
+    else:
+        print(f"All completed successfully! (Total: {len(overall_status_df)})")
+    return overall_status_df
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "root_path",
+        type=Path,
+        help="Root path of GM sim.ie. Parent directory of Runs or ObservedGroundMotions",
+    )
+    parser.add_argument("model", help="Name of the advanced IM model")
+    parser.add_argument(
+        "type",
+        choices=TYPES,
+        default="custom",
+        help="Type of data. If neither sim or obs, choose custom and provide --pattern",
+    )
+    parser.add_argument(
+        "--outdir", type=Path, default=Path.cwd(), help="Output directory"
+    )
+    parser.add_argument(
+        "--pattern",
+        default=None,
+        help='Status file search pattern for custom type. eg.) "Runs/*/*/IM_calc"',
+    )
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        default=False,
+        help="Print unfinished events summary",
+    )
+
+    args = parser.parse_args()
+
+    if not args.root_path.is_dir():
+        raise AssertionError("Provide a valid directory path")
+
+    if args.type == "custom":
+        if args.pattern is None:
+            raise AssertionError("Type custom must have a search pattern")
+        else:
+            args.pattern = args.pattern.strip(
+                "/"
+            )  # remove leading or trailing "/" (if any)
+    else:
+        if args.pattern is not None:
+            print(
+                f"Warning: Type {args.type} uses pre-defined search pattern - supplied one ignored"
+            )
+
+    process(
+        args.type, args.model, args.root_path, args.outdir, args.pattern, args.verbose
+    )
+
+
+if __name__ == "__main__":
+    main()
