585da396b4510cb866d7e8c77ca31f22d6a810de
James Paterson <james.paterson@canterbury.ac.nz>
2021-08-26 16:50:42 +1200

    Cut out some spam

--- IM_calculation/IM/read_waveform.py
+++ IM_calculation/IM/read_waveform.py
@@ -134,7 +134,7 @@ def read_waveforms(
     :param file_type:
     :return: a list of waveforms
     """
-    print("Reading waveforms in: {}".format(units))
+    # print("Reading waveforms in: {}".format(units))
     if file_type == "ascii":
         return read_ascii_folder(path, station_names, comp=comp, units=units)
     elif file_type == "binary":
