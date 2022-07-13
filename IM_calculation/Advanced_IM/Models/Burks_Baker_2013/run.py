Got the calculation working (correct rotd_values) - yet to decide how to save the output

IM_calculation/Advanced_IM/Models/Burks_Baker_2013/run.py
@@ -71,11 +71,10 @@ def main(comp_000: Path, comp_090: Path, rotd: bool, output_dir: Path):
                     for i in range(0,181):
                         theta = i*np.pi / 180
                         rotD_comp = accelerations[:,0]*np.cos(theta)+accelerations[:,1]*np.sin(theta)
-                        ag_rotd = rotD_comp*g #/ 100
 
                         displacements = (
                             intensity_measures.get_SDI_nd(
-                                ag_rotd, period, DT, z, alpha, dy, dt
+                                rotD_comp, period, DT, z, alpha, dy, dt
                             ) # Binlinear_Newmark_withTH(period,z,dy, alpha, accelerations/100*G,DT,dt)
                             #* 100  # Burks & Baker returns m, but output is stored in cm
                         )