65caff3732ee3bc1aaa3b01c76d7e0d3928c23ba
sungeunbae <sung.bae@canterbury.ac.nz>
2023-10-09 09:10:53 +1300

    Added a bit more debug

--- IM_calculation/IM/im_calculation.py
+++ IM_calculation/IM/im_calculation.py
@@ -538,24 +538,26 @@ def compute_measures_mpi(
         logger.info(f"SERVER: Total stations {len(station_names)} Stations previously computed {len(found_stations)} Stations to compute {len(stations_to_run)}")
         nworkers = size - 1
         while nworkers >= 0:
-            worker_id = comm.recv(source=MPI.ANY_SOURCE, status=status)
-            assert worker_id == status.Get_source()
+            logger.info(f"SERVER: listening")
+            worker_id = comm.recv(source=MPI.ANY_SOURCE, status=status, tag=1)
+            #assert worker_id == status.Get_source()
             logger.info(f"SERVER: rank_{worker_id} wants a job")
             # next job
             if len(stations_to_run) > 0:
                 station = stations_to_run.pop(-1)
                 logger.info(f"SERVER: Sending station {station} to rank_{worker_id}")
-                comm.send(obj=station, dest=worker_id)
+                comm.send(obj=station, dest=worker_id, tag=2)
             else:
                 logger.info(f"SERVER: No job to give. Tell rank_{worker_id} to stop")
-                comm.send(obj=None, dest=worker_id) #
+                comm.send(obj=None, dest=worker_id, tag=2) #
                 nworkers -= 1
         logger.info("SERVER: All stations complete")
     else:
         while True:
             logger.info(f"WORKER rank_{rank}: requesting a job")
-            comm.send(obj=rank, dest=server)
-            station = comm.recv(source=server)
+            comm.send(obj=rank, dest=server, tag=1)
+            logger.info(f"WORKER rank_{rank}: listening to the server")
+            station = comm.recv(source=server, tag=2)
             if station is None:
                 logger.info(f"WORKER rank_{rank}: was ordered to stop")
                 break
