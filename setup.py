01d411165e7d5815e7b91f0db4b6e9811631047a
joelridden <jriddenw@gmail.com>
2024-07-22 11:15:03 +1200

    adding pyproject

--- setup.py
+++ setup.py
@@ -1,39 +1,16 @@
 """
 Install using pip, e.g. pip install ./IM_Calculation
-use --no-deps to prevent re-installation of dependencies
-use -I to force re-install
 """
-import subprocess
-import sys
-
-from setuptools import find_packages, setup, Extension
-
-# The following is required to build the Cython extension
-# This ensures that Cython is installed before running the setup
-try:
-    from Cython.Distutils import build_ext
-except ModuleNotFoundError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "Cython"])
-    from Cython.Distutils import build_ext
+from Cython.Distutils import build_ext
+from setuptools import setup, Extension
 
 
 setup(
-    name="IM-calc",
-    version="19.5.1",
-    packages=find_packages(),
-    url="https://github.com/ucgmsim/IM_calculation",
-    description="IM calculation code",
-    install_requires=["Cython", "obspy", "pandas"],
     cmdclass={"build_ext": build_ext},
-    package_data={"": ["*.yaml"]},
     ext_modules=[
         Extension(
             "IM_calculation.IM.rspectra_calculations.rspectra",
             ["IM_calculation/IM/rspectra_calculations/rspectra.pyx"],
         ),
-    ],
-    scripts=[
-        "IM_calculation/scripts/calculate_ims.py",
-        "IM_calculation/scripts/calculate_rrups_single.py",
     ],
 )
