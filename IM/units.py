262e238c48911d3ff4320b1844e47d7a049de764
Jake Faulkner <jakefaulkn@gmail.com>
2025-01-27 14:27:27 +1300

    refactor(units + im_reader): remove im_reader, add units for periods + frequency

---
+++ IM/units.py
@@ -0,0 +1,16 @@
+from IM.ims import IM
+
+
+IM_UNITS = {
+    IM.PGA: "g0",
+    IM.PGV: "cm/s",
+    IM.CAV: "m/s",
+    IM.CAV5: "m/s",
+    IM.AI: "m/s",
+    IM.Ds575: "s",
+    IM.Ds595: "s",
+    IM.FAS: "g0 * s",
+    IM.pSA: "g0",
+    "frequency": "Hz",
+    "period": "s",
+}
