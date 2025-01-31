d0f05fc1ff28b1e39a7968d93618992eb848fc8d
joelridden <jriddenw@gmail.com>
2025-01-31 13:44:46 +1300

    remove numba num threads

--- IM/ims.py
+++ IM/ims.py
@@ -65,7 +65,7 @@ def set_cores(cores: int = multiprocessing.cpu_count()) -> None:
 
     # Set environment variables for Numba
     os.environ['NUMBA_MAX_THREADS'] = str(cores)
-    os.environ['NUMBA_NUM_THREADS'] = str(cores)
+    # os.environ['NUMBA_NUM_THREADS'] = str(cores)
 
     # Set the number of threads for OpenBLAS
     # Needed for matrix multiplication in NumPy during FAS
